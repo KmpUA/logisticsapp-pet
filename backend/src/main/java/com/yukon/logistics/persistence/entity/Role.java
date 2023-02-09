2487d4b737adfff66515b928ee1c8496477964c1
Denys Selin <selin.denys@chnu.edu.ua>
2023-02-09 15:47:26 +0200

    Added comments

--- backend/src/main/java/com/yukon/logistics/persistence/entity/Role.java
+++ backend/src/main/java/com/yukon/logistics/persistence/entity/Role.java
@@ -1,8 +1,28 @@
 package com.yukon.logistics.persistence.entity;
 
+/*
+ * Roles that are used for the implementation of security
+ * and can be used in the Role in the future.
+ */
 public enum Role {
-    ADMIN,
-    CUSTOMER,
+    /*
+     * Default role.
+     */
+    USER,
+    /*
+     * Specific role.
+     */
     TRUCKER,
-    DISPATCHER
+    /*
+     * Specific trucker role.
+     */
+    CUSTOMER,
+    /*
+     * Specific dispatcher role.
+     */
+    DISPATCHER,
+    /*
+     * Privilege role.
+     */
+    ADMIN
 }
