288a1b5a035a160d2e078a3fba0b06efe2208c4a
Denys Selin <selin.denys@chnu.edu.ua>
2023-02-15 11:46:28 +0200

    #40 Added @EntityListeners and @EnableJpaAuditing annotations

--- backend/src/main/java/com/yukon/logistics/persistence/entity/Order.java
+++ backend/src/main/java/com/yukon/logistics/persistence/entity/Order.java
@@ -2,6 +2,7 @@ package com.yukon.logistics.persistence.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,6 +19,7 @@ import lombok.experimental.FieldDefaults;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -27,6 +29,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Table(name = "orders")
+@EntityListeners(AuditingEntityListener.class)
 public class Order {
 
     @Id
