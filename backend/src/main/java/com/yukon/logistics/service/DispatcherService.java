c3d3efa3452d8a578ec8f10a1b8c65b130b44c10
Denys Selin <selin.denys@chnu.edu.ua>
2023-02-26 13:03:40 +0200

    Added missing search method

--- backend/src/main/java/com/yukon/logistics/service/DispatcherService.java
+++ backend/src/main/java/com/yukon/logistics/service/DispatcherService.java
@@ -5,6 +5,7 @@ import com.yukon.logistics.persistence.entity.Dispatcher;
 import java.util.List;
 
 public interface DispatcherService {
+    Dispatcher findDispatcherById(Long id);
     Dispatcher addDispatcher(Dispatcher dispatcher);
     List<Dispatcher> findAllDispatchers();
     Dispatcher updateDispatcher(Dispatcher dispatcher);
